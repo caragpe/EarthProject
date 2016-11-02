d334c91f77075572ceaaabdc2f781076a05cc41b
Carlos AP <carlos.agullo@gmail.com>
2016-11-02 11:56:46 +0000

    Starting tests with forms. Disabling owner validation

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,19 @@
 class PropertiesController < ApplicationController
+    def new
+        @property = Property.new
+    end
+    
+    def create
+        #render plain: params[:property].inspect
+        @property = Property.new(property_params)
+        @property.save
+        if @property.errors.any?
+            render plain: @property.errors.full_messages
+        end
+    end
+    
+    private
+    def property_params
+        params.require(:property).permit(:property_name,:description)
+    end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ActiveRecord::Base
     validates :property_name, presence: true, length: {minimum: 4, maximun: 30}
     validates :description, presence: true
-    validates :owner_id, presence: true
+    #validates :owner_id, presence: true
 end
