a1599890658b74e138e7a8febb7f0977a9bad1d1
Carlos AP <carlos.agullo@gmail.com>
2016-11-07 11:01:16 +0000

    Adding migration for User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102025915) do
+ActiveRecord::Schema.define(version: 20161107105118) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "property_name"
     t.text     "description"
     t.integer  "owner_id"
     t.float    "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
