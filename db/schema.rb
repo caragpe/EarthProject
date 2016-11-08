a4d90a32b94ec917de36688f3838d0f0868fa2ff
Carlos AP <carlos.agullo@gmail.com>
2016-11-08 02:47:59 +0000

    Add secure password to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107113319) do
+ActiveRecord::Schema.define(version: 20161108024328) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "property_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20161107113319) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
