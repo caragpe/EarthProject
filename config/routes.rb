e78cf419da76c4cb8a55d7327a2cd7f7d4ead7b2
Carlos AP <carlos.agullo@gmail.com>
2016-11-09 02:59:01 +0000

    Adding signup user ability

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
   resources :properties
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   #get 'pages/home', to: 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
