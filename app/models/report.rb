29d600c2e4405a21010d9022b051bd45c540aa70
Carlos AP <carlos.agullo@gmail.com>
2016-11-26 18:16:28 +0000

    Fixing Report Model: empty file crashed model logic

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,18 +2,20 @@ class Report < ActiveRecord::Base
     belongs_to :property
     validates :title, presence: true, length: { minimum: 6, maximum: 50 }
     validates :year, presence: true
-    validates :file_contents, presence: true 
+    validates :file_contents, presence: true
     
+    #validate :file_present
    
 
     
     #validates_size_of :file, maximum: 1.megabytes, message: "should be less than 1MB"
-    validate :file_size_under_two_mb
+    validate :file_size_under_two_mb, if: :file_present
     
     def initialize(params = {})
+        
         file = params.delete(:file)
         super
-        if file
+        if !file.nil?
             self.filename = sanitize_filename(file.original_filename)
             self.content_type = file.content_type
             self.file_contents = file.read
@@ -25,9 +27,14 @@ class Report < ActiveRecord::Base
     private
     
         NUM_BYTES_IN_MEGABYTE = 1048576
+
+        def file_present
+            !self.file_contents.nil?
+        end
         
         def file_size_under_two_mb
-            if (self.file_contents.size.to_f / NUM_BYTES_IN_MEGABYTE) > 2
+            
+            if ((self.file_contents.size.to_f / NUM_BYTES_IN_MEGABYTE) > 2) && !file_contents.nil?
                 errors.add(:file, 'size cannot be over two megabytes.')
             end
         end
